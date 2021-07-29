577997075d92e1c4b538249c291115e6f798905a
Alexandra Kostyuchenko <all2115@mail.ru>
2021-07-30 01:16:14 +0500

    New branch lesson001

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log('Hello');
-alert('hello');
+const money = 1; 
+let income = 2; 
+const addExpenses = 3; 
+let deposit = 4;
+const mission = 5;
+let period = 6;
+
+alert('Доброго времени суток!');
+console.log('Хорошего и позитивного настроения!');
